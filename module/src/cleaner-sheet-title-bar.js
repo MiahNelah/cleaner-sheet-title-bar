f4e5c03fca4d7b9fc8f91992adc3d7bc32e0557d
Miah Nelah <cbouyer@protonmail.com>
2022-12-14 13:45:17 +0100

    add compact navbars feature

--- module/src/cleaner-sheet-title-bar.js
+++ module/src/cleaner-sheet-title-bar.js
@@ -1,5 +1,5 @@
 function removeTextFromButton(element) {
-    if (element.title === undefined || element.title.trim() === "" )
+    if (element.title === undefined || element.title.trim() === "")
         element.title = element.innerText?.trim();
 
     // Maestro compatibility hack
@@ -20,7 +20,7 @@ function cleanDocumentHeader(app, html) {
 
     // When using PopOut! module, button text is reset when you pop window in.
     // In this case, html is just form and not all window. So, we find parent window to get header section
-    const header = ( "form".localeCompare(html[0].tagName, undefined, { sensitivity: 'base' }) === 0 )
+    const header = ("form".localeCompare(html[0].tagName, undefined, { sensitivity: 'base' }) === 0)
         ? html[0].parentElement.parentElement
         : html[0];
 
@@ -39,6 +39,30 @@ function cleanDocumentHeader(app, html) {
     }, 100);
 }
 
+function cleanSheetTabs(app, html) {
+    if (!(app instanceof TokenConfig)
+        || !html
+        || !game.settings.get("cleaner-sheet-title-bar", "collapse-navbars")) return;
+
+    const navs = html[0].querySelectorAll(".window-content nav.tabs");
+    if (navs === null || navs === undefined) return;
+
+    setTimeout(() => {
+        for (let nav of navs) {
+            const tabs = nav.querySelectorAll("a.item");
+            if (tabs === null
+                || tabs === undefined
+                || (Array.isArray(tabs) && !tabs.length)) return;
+
+            for (let tab of tabs) {
+                removeTextFromButton(tab);
+            }
+            $(nav).css("justify-content", "center");
+        }
+        app.setPosition(app.position);
+    }, 100);
+}
+
 function cleanPoppedDocumentHeader(app, poppedWindow) {
     if (poppedWindow === undefined) return;
     cleanDocumentHeader(app, $(poppedWindow));
@@ -50,18 +74,31 @@ function handlePopout() {
     }
 }
 
-
-Hooks.on('renderApplication', cleanDocumentHeader);
-Hooks.on('renderDocumentSheet', cleanDocumentHeader);
-Hooks.on('renderActorSheet', cleanDocumentHeader);
-Hooks.on('renderJournalSheet', cleanDocumentHeader);
-Hooks.on('renderItemSheet', cleanDocumentHeader);
-Hooks.on('renderRollTableConfig', cleanDocumentHeader);
-Hooks.on('renderSidebarTab', cleanDocumentHeader);
-Hooks.on('renderFormApplication', cleanDocumentHeader);
+function eventHandler(app, html) {
+    cleanDocumentHeader(app, html);
+    cleanSheetTabs(app, html);
+}
 
 
+Hooks.on('renderApplication', eventHandler);
+Hooks.on('renderDocumentSheet', eventHandler);
+Hooks.on('renderActorSheet', eventHandler);
+Hooks.on('renderJournalSheet', eventHandler);
+Hooks.on('renderItemSheet', eventHandler);
+Hooks.on('renderRollTableConfig', eventHandler);
+Hooks.on('renderSidebarTab', eventHandler);
+Hooks.on('renderFormApplication', eventHandler);
 
 Hooks.on('init', () => {
     handlePopout();
+    
+    game.settings.register("cleaner-sheet-title-bar", "collapse-navbars", {
+        name: game.i18n.localize('CSTB.Settings.CompactNavbars.Title'),
+        hint: game.i18n.localize('CSTB.Settings.CompactNavbars.Hint'),
+        scope: "world",
+        group: "UI",
+        config: true,
+        type: Boolean,
+        default: false,
+    });
 });
